82965df0c13de39e1fdeb0cc72eafa9e2367ad2c
Ben Freeman <ben@netsco.co.uk>
2026-01-13 12:17:47 +0100

    remove modal handling logic from passkey registration steps

--- src/Livewire/PasskeyAuthentication.php
+++ src/Livewire/PasskeyAuthentication.php
@@ -96,10 +96,6 @@ class PasskeyAuthentication extends PasskeysComponent implements HasActions, Has
                         $this->dispatch('passkeyPropertiesValidated', [
                             'passkeyOptions' => json_decode($this->generatePasskeyOptions()),
                         ]);
-
-                        // Keep the modal open while WebAuthn registration is in progress.
-                        // The modal will be closed by JavaScript after registration succeeds or fails.
-                        $action->halt();
                     }),
             ])
             ->columns([
@@ -144,17 +140,9 @@ class PasskeyAuthentication extends PasskeysComponent implements HasActions, Has
     {
         parent::storePasskey($passkey);
 
-        // Close the modal and refresh the table
-        $this->dispatch('close-modal', id: "{$this->getId()}-table-action");
-
         Notification::make()
             ->title(__('filament-two-factor-authentication::components.passkey.added'))
             ->success()
             ->send();
-    }
-
-    public function closePasskeyModal(): void
-    {
-        $this->dispatch('close-modal', id: "{$this->getId()}-table-action");
     }
 }
